912036406ec90242edbe8da05e482c8fa72a7d0d
Matrix-Monk <matrixmonk22@gmail.com>
2025-02-14 12:45:58 +0530

    Add about section

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
   
@@ -14,13 +14,29 @@ const Navbar = () => {
       sideMenuRef.current.style.transform = "translateX(16rem)";
     };
 
+    const [isScroll, setIsScroll] = useState(false)
+    
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (scrollY > 50) {
+                setIsScroll(true)
+            } else {
+                setIsScroll(false)
+            }
+        })
+    }, [])
+
 
     return (
       <>
         <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
           <Image src={assets.header_bg_color} alt="" className="w-full " />
         </div>
-        <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+        <nav
+          className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
+            isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""
+          }`}
+        >
           <a href="#top">
             <Image
               src={assets.logo}
@@ -29,7 +45,11 @@ const Navbar = () => {
             />
           </a>
 
-          <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shrink-sm bg-opacity-50">
+          <ul
+            className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
+              isScroll ? " bg-white shrink-sm bg-opacity-50" : ""
+            }`}
+          >
             <li>
               <a className="font-Ovo" href="#top">
                 Home
