89d88ab13000e462af861031362388f6286be5d5
Matrix-Monk <matrixmonk22@gmail.com>
2025-02-14 20:27:13 +0530

    Add dark mode feature

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[90%_auto]"
+      className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[90%_auto] dark:bg-none"
     >
       <h4 className="text-center mb-2 text-lg font-Ovo">Contact me</h4>
       <h2 className="text-center text-5xl font-Ovo">Get in touch</h2>
@@ -52,15 +52,15 @@ const Contact = () => {
             type="text"
             placeholder="Enter your name"
             required
-                      className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
-                      name="name"
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90"
+            name="name"
           />
           <input
             type="email"
             placeholder="Enter your email"
             required
-                      className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
-                      name="email"
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white  dark:bg-darkHover/30 dark:border-white/90"
+            name="email"
           />
         </div>
 
@@ -68,18 +68,18 @@ const Contact = () => {
           rows="6"
           placeholder="Type your message"
           required
-                  className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
-                  name="message"
+          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6  dark:bg-darkHover/30 dark:border-white/90"
+          name="message"
         ></textarea>
 
         <button
-          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500"
+          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:border-r-darkHover"
           type="submit"
         >
           Send an Email{" "}
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
         </button>
-              <p className="mt-4">{ result}</p>
+        <p className="mt-4">{result}</p>
       </form>
     </div>
   );
--- app/layout.js
+++ app/layout.js
@@ -18,12 +18,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth dark">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-auto` }
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
-      </body>  
+      </body>
     </html>
   );
 }
+//dark
+
+
+// dark:bg-darkTheme dark:text-white
